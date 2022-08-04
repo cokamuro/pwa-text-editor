f8e7358e4d07ff3b16a42b0cd0035ccfa2546d77
Chris Okamuro <cokamuro@gmail.com>
2022-08-03 21:10:03 -0400

    db operations

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// DONE: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const transaction = db.transaction('jate', 'readwrite');
+  const obStore = transaction.objectStore('jate');
+  
+  //return 0 if empty
+  const count = (await obStore.get('counter')) || 0;
+  count++
+  const result = await obStore.put({ id: count, value: content });
+  
+  await transaction.done;
+  
+  return result;
+};
+
+// DONE: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+
+  const transaction = db.transaction('jate', 'readwrite');
+  const obStore = transaction.objectStore('jate');
+  
+  const result = await obStore.getAll();
+
+  await transaction.done;
+
+  return result.value;
+}
 
 initdb();
